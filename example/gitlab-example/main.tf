f9e8ffcd9a8d00c69c24019f044e2ca849aa9571
jomcy <jomcy@amazon.com>
2022-02-07 05:10:34 +0530

    gitlab.ci added to project root

--- example/gitlab-example/main.tf
+++ example/gitlab-example/main.tf
@@ -2,6 +2,6 @@ module "wm-gitlab-demo" {
   source = "git::https://github.com/jomcy-amzn/wm-serverlesslambda.git"
 }
 
-output "wm-get_url" {
+output "wm_get_url" {
   value = module.wm-gitlab-demo.wm_read_url
 }
